974692f4ea201af806a5853f3f923af9dffa8634
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2025-07-09 12:58:08 +0100

    feat: installed sass using npm

---
+++ 0x03-sass_scss/0-installation-script.sh
@@ -0,0 +1,11 @@
+# This script installs Sass globally on your system and provides instructions for using it.
+npm install -g sass
+
+#the command below helps to check vaersion of sass installed
+sass --version
+
+# To watch the file and compile it automatically
+# The command below will watch the input.scss file and compile it to output.css
+# You can run this command in the terminal to see the changes in real-time
+# Make sure you are in the correct directory where the input.scss file is located
+#sass --watch 0x03-sass_scss/src/input.scss:0x03-sass_scss/src/output.css
